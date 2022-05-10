e9f51aff8d5f4aabd6815bf82af4704014d9c568
kim-pink-ba <kimpinkba@gmail.com>
2022-05-10 18:30:15 +0800

    feat: Migrate all config/hooks/utils

--- src/contexts/Localization/types.ts
+++ src/contexts/Localization/types.ts
@@ -1,6 +1,8 @@
 import { ReactText } from 'react'
 import { Language } from '@pancakeswap/uikit'
 
+import translations from 'config/localization/translations.json'
+
 export type ContextData = {
   [key: string]: ReactText
 }
@@ -16,3 +18,8 @@ export interface ContextApi extends ProviderState {
 }
 
 export type Translate = (key: string, data?: ContextData) => string
+
+// To support string literals and union of string
+// https://stackoverflow.com/questions/61047551/typescript-union-of-string-and-string-literals
+type MaybeObject = Record<never, never>
+export type TranslationKey = keyof typeof translations | (string & MaybeObject)
