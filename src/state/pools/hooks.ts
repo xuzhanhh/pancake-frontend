951e1f7099474071ffa73e50e79df460905d1f74
xuzhanhh <xuzhanhh@gmail.com>
2022-07-04 11:54:13 +0800

    feat: Add performance update

--- src/state/pools/hooks.ts
+++ src/state/pools/hooks.ts
@@ -20,6 +20,19 @@ import {
   makeVaultPoolByKey,
   poolsWithVaultSelector,
 } from './selectors'
+import { livePools } from 'config/constants/pools'
+
+const lPoolAddresses = livePools.filter(({ sousId }) => sousId !== 0).map(({ earningToken }) => earningToken.address)
+
+// Only fetch farms for live pools
+const activeFarms = farmsConfig
+  .filter(
+    ({ token, pid, quoteToken }) =>
+      pid !== 0 &&
+      ((token.symbol === 'BUSD' && quoteToken.symbol === 'WBNB') ||
+        lPoolAddresses.find((poolAddress) => poolAddress === token.address)),
+  )
+  .map((farm) => farm.pid)
 
 export const useFetchPublicPoolsData = () => {
   const dispatch = useAppDispatch()
@@ -27,8 +40,7 @@ export const useFetchPublicPoolsData = () => {
   useSlowRefreshEffect(
     (currentBlock) => {
       const fetchPoolsDataWithFarms = async () => {
-        const activeFarms = farmsConfig.filter((farm) => farm.pid !== 0)
-        await dispatch(fetchFarmsPublicDataAsync(activeFarms.map((farm) => farm.pid)))
+        await dispatch(fetchFarmsPublicDataAsync(activeFarms))
         batch(() => {
           dispatch(fetchPoolsPublicDataAsync(currentBlock))
           dispatch(fetchPoolsStakingLimitsAsync())
