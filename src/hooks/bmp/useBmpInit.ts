54626f3d27359552b79e7b9bc2bd0e7ca3d2492d
opar-xu <opar-xu>
2022-07-12 18:32:08 +0800

    fix: Fix tabbar i18n

--- src/hooks/bmp/useBmpInit.ts
+++ src/hooks/bmp/useBmpInit.ts
@@ -9,6 +9,7 @@ const useBmpInit = () => {
   /*                           Set Tab Item I18N Text                           */
   /* -------------------------------------------------------------------------- */
   useEffect(() => {
+    console.log('~ setTabBarItem')
     mpService.setTabBarItem({
       index: 0,
       text: t('Exchange'),
@@ -16,6 +17,14 @@ const useBmpInit = () => {
     mpService.setTabBarItem({
       index: 1,
       text: t('Liquidity'),
+    })
+    mpService.setTabBarItem({
+      index: 2,
+      text: t('Farm'),
+    })
+    mpService.setTabBarItem({
+      index: 3,
+      text: t('Pools'),
     })
   }, [t, isFetching])
 }
