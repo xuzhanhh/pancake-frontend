84358c7c55361d0e6dc2b1688dcb615ac8645bc0
Zane X <zane.x@binance.com>
2022-07-25 11:51:46 +0800

    feat: Add warning token info

--- src/views/Swap/components/SwapWarningModal/index.bmp.tsx
+++ src/views/Swap/components/SwapWarningModal/index.bmp.tsx
@@ -10,6 +10,9 @@ import ItamWarning from './ItamWarning'
 import BondlyWarning from './BondlyWarning'
 import Acknowledgement from './Acknowledgement'
 import CcarWarning from './CcarWarning'
+import BTTWarning from './BTTWarning'
+import RugPullWarning from './RugPullWarning'
+import FREEWarning from './FREEWarning'
 
 const StyledModalContainer = styled(ModalContainer)`
   max-width: 440px;
@@ -47,6 +50,18 @@ const SwapWarningModal: React.FC<SwapWarningModalProps> = ({ swapCurrency, onDis
     [SwapWarningTokensConfig.ccar.address]: {
       symbol: SwapWarningTokensConfig.ccar.symbol,
       component: <CcarWarning />,
+    },
+    [SwapWarningTokensConfig.bttold.address]: {
+      symbol: SwapWarningTokensConfig.bttold.symbol,
+      component: <BTTWarning />,
+    },
+    [SwapWarningTokensConfig.pokemoney.address]: {
+      symbol: SwapWarningTokensConfig.pokemoney.symbol,
+      component: <RugPullWarning />,
+    },
+    [SwapWarningTokensConfig.free.address]: {
+      symbol: SwapWarningTokensConfig.free.symbol,
+      component: <FREEWarning />,
     },
   }
 
